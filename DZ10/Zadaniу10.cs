a868950e32ee85677d1bac7de7099bd477a0a6a4
mO-Orfy <113131414+mO-Orfy@users.noreply.github.com>
2023-09-14 22:18:45 +0300

    Add files via upload

---
+++ DZ10/Zadaniу10.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите длину окружности");
+double l = Convert.ToDouble(Console.ReadLine());
+double S = Math.Pow((l / 2), 2) / Math.PI;
+Console.WriteLine($"Площадь вписанного круга: {S}");
