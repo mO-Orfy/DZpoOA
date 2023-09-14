9ce3db63f9eac20a48ce2f59df6756d7a32933d4
mO-Orfy <113131414+mO-Orfy@users.noreply.github.com>
2023-09-14 22:24:05 +0300

    Delete DZ/Zadaniу11.cs

--- DZ/Zadaniу11.cs
+++
@@ -1,6 +0,0 @@
-﻿Console.WriteLine("Введите внутренний радиус (r):");
-double r = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите внешний радиус (R):");
-double R = Convert.ToInt32(Console.ReadLine());
-double S = Math.PI * (Math.Pow(R, 2) - Math.Pow(r, 2));
-Console.WriteLine($"Площадь кольца: {S}");
