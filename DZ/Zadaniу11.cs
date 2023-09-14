6c6069948958d454158484db9ee0777285effc02
mO-Orfy <113131414+mO-Orfy@users.noreply.github.com>
2023-09-14 22:16:55 +0300

    Add files via upload

---
+++ DZ/Zadaniу11.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите внутренний радиус (r):");
+double r = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите внешний радиус (R):");
+double R = Convert.ToInt32(Console.ReadLine());
+double S = Math.PI * (Math.Pow(R, 2) - Math.Pow(r, 2));
+Console.WriteLine($"Площадь кольца: {S}");
---
+++ DZ/Zadaniу3.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите радиус круга");
+double radius = Convert.ToDouble(Console.ReadLine());
+double S = Math.PI * radius * radius;
+double C = 2 * Math.PI * radius;
+Console.WriteLine($"S = {S}, C = {C}");
